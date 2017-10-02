92d3df3149dec0a78e75a65825778540f733c692
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-02 14:41:55 -0600

    add 4 negative test cases, similar to positive test cases

--- test/edu/unm/cs583/VelocityTester.java
+++ test/edu/unm/cs583/VelocityTester.java
@@ -29,6 +29,7 @@ public class VelocityTester {
 	public void testReverseY()	{
 		Velocity v;
 		
+		// positive test cases
 		v = new Velocity(0, 90);
 		v.reverseY();
 		assertEquals(0, v.getSpeedY());
@@ -52,6 +53,28 @@ public class VelocityTester {
 		v = new Velocity(100, 480);
 		v.reverseY();
 		assertEquals(-90, v.getSpeedY());
+		assertEquals(-120, v.getDirection());
+		
+		
+		// negative test cases
+		v = new Velocity(-100, 0);
+		v.reverseY();
+		assertEquals(0, v.getSpeedY());
+		assertEquals(0, v.getDirection());
+		
+		v = new Velocity(-100, 90);
+		v.reverseY();
+		assertEquals(99, v.getSpeedY());
+		assertEquals(270, v.getDirection());
+		
+		v = new Velocity(-100, 360);
+		v.reverseY();
+		assertEquals(-6, v.getSpeedY());
+		assertEquals(0, v.getDirection());
+		
+		v = new Velocity(-100, 480);
+		v.reverseY();
+		assertEquals(90, v.getSpeedY());
 		assertEquals(-120, v.getDirection());
 	}
 
