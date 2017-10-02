a26551a3dca8edf64115469f95d53b122f8eb11b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-02 17:37:13 -0600

    fix minor bug in Velocity constructor

--- src/edu/unm/cs583/Velocity.java
+++ src/edu/unm/cs583/Velocity.java
@@ -24,7 +24,7 @@ class Velocity {
      *@param newDirection Defines the initial direction
      */
     public Velocity(int newSpeed, int newDirection) {
-        this.setDirection(direction);
+        this.setDirection(newDirection);
         this.setSpeed(newSpeed);
         decomposeSpeed();
     }
