f76c9d16ea9ebb4ea4bc6a44ef91675e5a69fca4
davi0011 <davi0011@users.noreply.github.com>
2017-10-01 22:00:38 -0600

    Add files via upload
    Committing 5 positive tests for the speed functions in VelocityTester.java

--- test/edu/unm/cs583/VelocityTester.java
+++ test/edu/unm/cs583/VelocityTester.java
@@ -17,12 +17,64 @@ public class VelocityTester {
 	}
 
 	@Test
-	/** public void test() {
-		fail("Not yet implemented"); **/
+	/**
+	 * public void test() { fail("Not yet implemented");
+	 **/
 	public void testSetDirection() {
 		Velocity out = new Velocity();
-		   out.setDirection(45);
-		   assertEquals("Result", 45, out.getDirection());
-		}
+		out.setDirection(45);
+		assertEquals("Result", 45, out.getDirection());
+	}
 
+	@Test
+	public void testSetSpeed() {
+		Velocity v = new Velocity(2, 0);
+		int expected = 5;
+		v.setSpeed(5);
+		assertEquals(expected, v.speed);
+		assertEquals(expected, v.getSpeedX());
+		assertEquals(0, v.getSpeedY());
+	}
+
+	@Test
+	public void testGetSpeedX() {
+		Velocity v = new Velocity(2, 0);
+		assertEquals(2, v.getSpeedX());
+
+		Velocity v2 = new Velocity(360, 90);
+		assertEquals(5, v2.getSpeedX());
+
+	}
+
+	@Test
+	public void testGetSpeedY() {
+		Velocity v = new Velocity(2, 0);
+		assertEquals(0, v.getSpeedY());
+
+		Velocity v2 = new Velocity(360, 90);
+		assertEquals(359, v2.getSpeedY());
+	}
+	
+	@Test
+	public void testDecomposeSpeed()
+	{
+		Velocity v = new Velocity();
+		assertEquals(0, v.getSpeedX());
+		assertEquals(0, v.getSpeedY());
+		
+		v.setSpeed(360);
+		assertEquals(360, v.getSpeedX());
+		assertEquals(0, v.getSpeedY());
+		
+		v.setDirection(90);
+		v.decomposeSpeed();
+		assertEquals(5, v.getSpeedX());
+		assertEquals(359, v.getSpeedY());
+		
+		v.setDirection(200);
+		v.decomposeSpeed();
+		assertEquals(-342, v.getSpeedX());
+		assertEquals(-110, v.getSpeedY());
+		
+	}
 }
